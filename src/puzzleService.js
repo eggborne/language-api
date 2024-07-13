3ada3c272450bc299999458a08ca512b5c03c790
eggborne <mike@mikedonovan.dev>
2024-07-13 11:00:01 -0700

    Add config.json

--- src/puzzleService.js
+++ src/puzzleService.js
@@ -1,11 +1,7 @@
 const { Trie } = require('./scripts/trie');
+const config = require('./config.json');
 const path = require('path');
 const fs = require('fs');
-
-// const WORD_LIST_PATH = path.join(__dirname, '..', 'wordsonlylist.json');
-
-const WORD_LIST_PATH = path.join(__dirname, 'wordsonlylist.json');
-const BOGGLE_LETTER_KEY = { '0': '', '1': 'In', '2': 'Th', '3': 'Er', '4': 'He', '5': 'An', 'Q': 'Qu' };
 
 const randomInt = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 const convertMatrix = (matrix, key = BOGGLE_LETTER_KEY) => {
@@ -16,34 +12,13 @@ const convertMatrix = (matrix, key = BOGGLE_LETTER_KEY) => {
   );
   return convertedMatrix;
 };
-const unconvertMatrix = (matrix, key = BOGGLE_LETTER_KEY) => {
-  const reversedKey = Object.fromEntries(
-    Object.entries(key).map(([k, v]) => [v, k])
-  );
-  const unconvertedMatrix = matrix.map(row =>
-    row.map(cell => {
-      return Object.prototype.hasOwnProperty.call(reversedKey, cell) ? reversedKey[cell] : cell;
-    })
-  );
-  return unconvertedMatrix;
-};
 
-const letterFrequencies = {
-  standardEnglish: { A: 42, B: 10, C: 23, D: 17, E: 56, F: 9, G: 12, H: 15, I: 38, J: 1, K: 6, L: 27, M: 15, N: 33, O: 36, P: 16, Q: 1, R: 38, S: 29, T: 35, U: 18, V: 5, W: 6, X: 1, Y: 9, Z: 1 },
-  modifiedEnglish: { A: 5, E: 5, I: 5, O: 5, U: 5, B: 3, C: 4, D: 3, F: 3, G: 3, H: 3, J: 1, K: 2, L: 4, M: 3, N: 4, P: 3, Q: 1, R: 4, S: 4, T: 4, V: 2, W: 2, X: 1, Y: 2, Z: 1 },
-  scrabble: { A: 9, B: 2, C: 2, D: 4, E: 12, F: 2, G: 3, H: 2, I: 9, J: 1, K: 1, L: 4, M: 2, N: 6, O: 8, P: 2, Q: 1, R: 6, S: 4, T: 6, U: 4, V: 2, W: 2, X: 1, Y: 2, Z: 1 },
-  wordsWithFriends: { A: 9, B: 2, C: 2, D: 5, E: 13, F: 2, G: 3, H: 4, I: 8, J: 1, K: 1, L: 4, M: 2, N: 5, O: 8, P: 2, Q: 1, R: 6, S: 5, T: 7, U: 4, V: 2, W: 2, X: 1, Y: 2, Z: 1 },
-  random: Object.fromEntries('ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('').map(letter => [letter, 10])),
-};
+const WORD_LIST_PATH = path.join(__dirname, config.wordListFileName);
+const BOGGLE_LETTER_KEY = config.letterKeys.boggle;
+const letterFrequencies = config.letterFrequencies;
+const boggleCubeSets = config.cubeSets.boggle;
 
-const boggleCubeSets = {
-  0: [], 1: [], 2: [], 3: [],
-  regular: ['AAEEGN', 'ABBJOO', 'ACHOPS', 'AFFKPS', 'AOOTTW', 'CIMOTU', 'DEILRX', 'DELRVY', 'DISTTY', 'EEGHNW', 'EEINSU', 'EHRTVW', 'EIOSST', 'ELRTTY', 'HIMNUQ', 'HLNNRZ'],
-  big: ['AAAFRS', 'AAEEEE', 'AAFIRS', 'ADENNN', 'AEEEEM', 'AEEGMU', 'AEGMNN', 'AFIRSY', 'BJKQXZ', 'CCENST', 'CEIILT', 'CEILPT', 'CEIPST', 'DDHNOT', 'DHHLOR', 'DHLNOR', 'DHLNOR', 'EIIITT', 'EMOTTT', 'ENSSSU', 'FIPRSY', 'GORRVW', 'IPRRRY', 'NOOTUW', 'OOOTTU'],
-  superBig: ['AAAFRS', 'AAEEEE', 'AAEEOO', 'AAFIRS', 'ABDEIO', 'ADENNN', 'AEEEEM', 'AEEGMU', 'AEGMNN', 'AEILMN', 'AEINOU', 'AFIRSY', 'Q12345', 'BBJKXZ', 'CCENST', 'CDDLNN', 'CEIITT', 'CEIPST', 'CFGNUY', 'DDHNOT', 'DHHLOR', 'DHHNOW', 'DHLNOR', 'EHILRS', 'EIILST', 'EILPST', 'EIORST', 'EMTTTO', 'ENSSSU', 'GORRVW', 'HIRSTV', 'HOPRST', 'IPRSYY', 'JKQWXZ', 'NOOTUW', 'OOOTTU']
-};
-
-const letterListfromFrequencyMap = (frequencyMap) => {
+const letterPoolFromFrequencyMap = (frequencyMap) => {
   let letterList = [];
   for (let letter in frequencyMap) {
     for (let i = 0; i < frequencyMap[letter]; i++) {
@@ -52,6 +27,14 @@ const letterListfromFrequencyMap = (frequencyMap) => {
   }
   return letterList;
 };
+
+const weightedLetterPools = {
+  scrabble: letterPoolFromFrequencyMap(letterFrequencies['scrabble']),
+  wordsWithFriends: letterPoolFromFrequencyMap(letterFrequencies['wordsWithFriends']),
+  standardEnglish: letterPoolFromFrequencyMap(letterFrequencies['standardEnglish']),
+  modifiedEnglish: letterPoolFromFrequencyMap(letterFrequencies['modifiedEnglish']),
+  random: letterPoolFromFrequencyMap(letterFrequencies['random']),
+}
 
 const letterListFromCubeSet = (cubeSet) => {
   return cubeSet.map(cube => cube[randomInt(0, cube.length - 1)]);
@@ -72,16 +55,20 @@ const getLetterList = (letterData, dimensions) => {
     letterList = letterListFromCubeSet(cubeSet);
   } else {
     if (letterData === 'boggle') {
-      letterList = letterListfromFrequencyMap(letterFrequencies['standardEnglish']);
+      letterList = weightedLetterPools['standardEnglish'];
     } else {
-      letterList = letterListfromFrequencyMap(letterFrequencies[letterData]);
+      letterList = weightedLetterPools[letterData];
     }
   }
   shuffleArray(letterList);
+  letterList = letterList.slice(0, width * height);
   return letterList;
 };
 
+let trie;
+
 const fetchWords = async () => {
+  console.warn('------ FETCHING WORD LIST');
   const data = fs.readFileSync(WORD_LIST_PATH, 'utf8');
   return JSON.parse(data);
 };
@@ -91,6 +78,17 @@ const initializeTrie = async (wordList) => {
   wordList.forEach(word => word.length > 2 && trie.insert(word));
   return trie;
 };
+
+const buildDictionary = async () => {
+  if (!trie) {
+    console.warn('------ FETCHING WORDS AND BUILDING TRIE');
+    const wordList = await fetchWords();
+    trie = await initializeTrie(wordList);
+  } else {
+    console.warn('Reusing built trie')
+  }
+  return trie;
+}
 
 const findAllWords = (options, matrix, trie) => {
   const { maximumPathLength } = options;
@@ -102,7 +100,6 @@ const findAllWords = (options, matrix, trie) => {
   const rows = matrix.length;
   const cols = matrix[0].length;
 
-  // Pre-allocate visited array
   const visited = new Array(rows);
   for (let i = 0; i < rows; i++) {
     visited[i] = new Array(cols).fill(false);
@@ -138,7 +135,6 @@ const findAllWords = (options, matrix, trie) => {
     visited[x][y] = false;
   };
 
-  // Iterate through the matrix to start searches from each cell
   for (let i = 0; i < rows; i++) {
     for (let j = 0; j < cols; j++) {
       dfs(i, j, '', trie.root);
@@ -173,7 +169,7 @@ const isPuzzleValid = (wordList, options) => {
     // console.warn('totalWordLimit OK')
   }
 
-  if (Object.entries(options.wordLengthLimits).length > 0) {
+  if (options.wordLengthLimits && Object.entries(options.wordLengthLimits).length > 0) {
     for (const [wordLength, limits] of Object.entries(options.wordLengthLimits)) {
       const { min = 0, max = Infinity } = limits;
       const wordsOfLength = wordList.filter(word => word.length === parseInt(wordLength)).length;
@@ -216,10 +212,8 @@ const generateBoard = async (options) => {
       const { dimensions, letterDistribution, letters } = options;
       const letterList = letters ? letters.split('') : getLetterList(letterDistribution, dimensions);
       const matrix = generateLetterMatrix(letterList, dimensions.width, dimensions.height);
-      const fullWordList = await fetchWords();
-      const trie = await initializeTrie(fullWordList);
+      await buildDictionary();
       const wordList = Array.from(findAllWords(options, matrix, trie)).sort((a, b) => a.length - b.length);
-
       if (isPuzzleValid(wordList, options)) {
         console.log(`Found valid puzzle after ${attempts} attempts`);
         return { matrix, wordList };
